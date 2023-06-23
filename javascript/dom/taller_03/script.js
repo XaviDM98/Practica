95befb46e237cab11982b9b5f4ec6facef633a91
Xavier Disintonio <134023573+XaviDM98@users.noreply.github.com>
2023-06-23 18:08:23 -0500

    Update script.js

--- javascript/dom/taller_03/script.js
+++ javascript/dom/taller_03/script.js
@@ -1,7 +1,7 @@
 function cargar_elemento() {
     var container = document.getElementById('container')
     var h1 = document.createElement('h1')
-    var texto = document.createTextNode('Guillermo Pizarro')
+    var texto = document.createTextNode('Xavier Disintonio')
     h1.appendChild(texto)
     container.appendChild(h1)
 }
