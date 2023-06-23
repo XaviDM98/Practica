0cc79fd86915ab5d1770be80e62097fc27f52bf3
Xavier Disintonio <134023573+XaviDM98@users.noreply.github.com>
2023-06-23 18:18:54 -0500

    Delete javascript/dom/taller_01 directory

--- javascript/dom/taller_01/script.js
+++
@@ -1,7 +0,0 @@
-function cargar_elemento() {
-    var container = document.getElementById('container')
-    var h1 = document.createElement('h1')
-    var texto = document.createTextNode('Xavier Disintonio')
-    h1.appendChild(texto)
-    container.appendChild(h1)
-}
