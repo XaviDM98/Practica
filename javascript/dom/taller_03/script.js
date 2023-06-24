d8df9c9f7ebbbb35800a26538054d14e61c2382e
XaviDM98 <xavidonald.999@gmail.com>
2023-06-23 19:22:36 -0500

    X

--- javascript/dom/taller_03/script.js
+++ javascript/dom/taller_03/script.js
@@ -1,7 +1,7 @@
 function cargar_elemento() {
     var container = document.getElementById('container')
     var h1 = document.createElement('h1')
-    var texto = document.createTextNode('Xavier Disintonio')
+    var texto = document.createTextNode('Xavier Disintonio M')
     h1.appendChild(texto)
     container.appendChild(h1)
 }
