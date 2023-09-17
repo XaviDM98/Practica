b7a9ebedf5ffec0c06887a0a4273c018083e7899
XaviDM98 <xavidonald.999@gmail.com>
2023-09-16 23:45:48 -0500

    Update

--- PracticaCode/nodejs/taller_02/config.js
+++ PracticaCode/nodejs/taller_02/config.js
@@ -1,6 +1,7 @@
 const config = {
     PORT: 3000,
-    DB_URL: 'mongodb+srv://gpizarro:inexcelsisdeo@cluster0.turgu5m.mongodb.net/?retryWrites=true&w=majority'
+    DB_URL: 'mongodb+srv://xdisintonio:demilovato123@cluster0.lht8b2f.mongodb.net/?retryWrites=true&w=majority'
+    
 }
 
 module.exports = config
